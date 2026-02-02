ee500cced1bc56d48aa6d397d1b266f61cb6d432
tinaafriyie <tintenktinash@gmail.com>
2026-02-02 10:27:20 +0100

    Second Commit

---
+++ src/main/java/fr/isen/java2/db/daos/GenreDao.java
@@ -0,0 +1,20 @@
+package fr.isen.java2.db.daos;
+
+import java.util.List;
+
+import fr.isen.java2.db.entities.Genre;
+
+public class GenreDao {
+
+	public List<Genre> listGenres() {
+		throw new RuntimeException("Method is not yet implemented");
+	}
+
+	public Genre getGenre(String name) {
+		throw new RuntimeException("Method is not yet implemented");
+	}
+
+	public void addGenre(String name) {
+		throw new RuntimeException("Method is not yet implemented");
+	}
+}
